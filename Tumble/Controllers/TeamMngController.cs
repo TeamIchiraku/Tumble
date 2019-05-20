08c110b475c292429dbbff4474c8be73c586ff81
dhoyos6 <43800887+dhoyos6@users.noreply.github.com>
2019-05-19 21:56:31 -0700

    Added Team management controller and pic reference

---
+++ Tumble/Controllers/TeamMngController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tumble.Controllers
+{
+    public class TeamMngController : Controller
+    {
+        // GET: TeamMng
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
