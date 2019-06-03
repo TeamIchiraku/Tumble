7ff0bf45455a99e979b4502956f25071a922a6d3
nathaniel.oakes10 <nathaniel.oakes10@outlook.com>
2019-06-03 15:10:29 -0700

    Single task changed from picture to an object; needs to have more features about the object like a save option

--- Tumble/Controllers/TaskController.cs
+++ Tumble/Controllers/TaskController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Tumble.Models;
 using System.Web.Mvc;
 
 namespace Tumble.Controllers
@@ -16,7 +17,21 @@ namespace Tumble.Controllers
 
         public ActionResult SingleTask()
         {
-            return View();
+            var myData = new SingleTask
+            {
+                TaskName = "Food Prep",
+                TaskDescription = "blah balh blah balh balh blah balh blah balh balh",
+                EventDate = DateTime.Parse("7/22/2019 1:30:00 PM",
+                          System.Globalization.CultureInfo.InvariantCulture),
+                Req1 = "Something",
+                Req2 = "Something",
+                TotalNumber = 2,
+                NumberFilled = 1,
+                FAQDoc = "Food Prep"
+
+
+            };
+            return View(myData);
         }
     }
 }
---
+++ Tumble/Models/SingleTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tumble.Models
+{
+    public class SingleTask
+    {
+        public string TaskName;
+        public DateTime EventDate;
+        public string TaskDescription;
+        public string Req1;
+        public string Req2;
+        public int NumberFilled;
+        public int TotalNumber;
+        public string FAQDoc;
+
+    }
+}
